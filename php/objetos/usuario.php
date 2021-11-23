a3a043686b8561e501065b4beacad6e5eee2c832
Neekomata <82986795+Neekomata@users.noreply.github.com>
2021-11-23 15:40:15 -0300

    Cargue las funciones Login y Registrar, aun no los probe, pero me quedo sin bateria y me voy a casa en 5m uwu, los quiero bruh, vamo que se puede

--- php/objetos/usuario.php
+++ php/objetos/usuario.php
@@ -10,39 +10,67 @@ class Usuario{
         $this-> clave = $clave;
         $this-> puntos = 0;
     }
-}
 
     //Registro de usuario
     function login($usuario, $clave){
-        session_start();
-        $cnn = mysqli_connect("localhost", "root", "", "usuario");
+        try {
+            session_start();
+            $cnn = mysqli_connect("localhost", "root", "", "uno");
 
-        if($cnn-> connect_error){
-            die ("Error al conectar". $cnn-> connect_error);
+            //Si se conecta...
+            $sql = mysqli_query($cnn, "SELECT usuario FROM usuario WHERE user = '$usuario' AND clave = '$clave'");
+            $registro = mysqli_fetch_array($sql);
+
+            if($registro['user'] == $usuario){
+                mysqli_close($cnn);
+                $_SESSION['user'] = $usuario;
+                $_SESSION['login'] = true;
+                header('location: ../menu.php');
+            }else{
+                //Mostra mensaje de que los datos no coinciden
+            }
+        } catch (Exception $e) {
+        
+        echo "<script>alert('Error al conectar: '". $e->getMessage() .")</script>";
         }
-
-        //Si se conecta...
-        $sql = mysqli_query($cnn, "SELECT usuario FROM usuario WHERE user = '$usuario' AND clave = '$clave'");
-        $registro = mysqli_fetch_array($sql);
-
-        if($registro['user'] == $usuario){
-            mysqli_close($cnn);
-            $_SESSION['user'] = $usuario;
-            $_SESSION['login'] = true;
-            header('location: ../menu.php');
-        }else{
-            //Mostra mensaje de que los datos no coinciden
-        }
+        
 
     } 
 
     //Logeo de usuario
-    function registro($nombre, $clave, $confClave){
-        
+    function registro($usuario, $clave, $confClave){
+        //Si se conecta...
+        try {
+            
+            $cnn = mysqli_connect("localhost", "root", "", "uno");
+            $sql = mysqli_query($cnn, "SELECT usuario FROM usuario WHER user = '$usuario'");
+            $registro = mysqli_fetch_array($sql);
+
+             //Si hay coincidencia, ya existe el usuario
+            if($registro['user'] == $usuario){
+                //Mostrar mensaje de que ya existe el usuario
+            }else{
+                if($clave != $confClave){
+                    echo "<script>alert('Claves distintas')</script>";
+                }else{
+                    try {
+                        $sql = mysqli_query($cnn, "INSERT INTO usuario VALUES ('$usuario', '$clave', 0)");
+                        header('location: ../index.html');
+                
+                    }catch (Exception $e) {
+                        echo "<script>alert('Error al conectar: '". $e->getMessage() .")</script>";
+                        }//Catch
+                }
+            }
+                
+        } catch (Exception $e) {
+            echo "<script>alert('Error al conectar: '". $e->getMessage() .")</script>";
+            }//Catch
     }
 
     //Mostrar usuario
     function mostrarUsuario(){
         
     }
+}
 ?>
