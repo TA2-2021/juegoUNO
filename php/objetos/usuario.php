49b938cdb352c549e3e15ed006b8100f75162a1b
Neekomata <82986795+Neekomata@users.noreply.github.com>
2021-11-23 15:04:28 -0300

    Agregue funcion Logear en usuario.php

--- php/objetos/usuario.php
+++ php/objetos/usuario.php
@@ -5,21 +5,44 @@ class Usuario{
     protected $clave;
     protected $puntos;
 
-    public function __construct($nombre, $clave, $puntos){
+    public function __construct($nombre, $clave){
         $this-> nombre = $nombre;
         $this-> clave = $clave;
-        $this-> puntos = $puntos;
+        $this-> puntos = 0;
     }
 }
 
     //Registro de usuario
-    function registrar($nombre, $clave, $puntos){
+    function login($usuario, $clave){
+        session_start();
+        $cnn = mysqli_connect("localhost", "root", "", "usuario");
+
+        if($cnn-> connect_error){
+            die ("Error al conectar". $cnn-> connect_error);
+        }
+
+        //Si se conecta...
+        $sql = mysqli_query($cnn, "SELECT usuario FROM usuario WHERE user = '$usuario' AND clave = '$clave'");
+        $registro = mysqli_fetch_array($sql);
+
+        if($registro['user'] == $usuario){
+            mysqli_close($cnn);
+            $_SESSION['user'] = $usuario;
+            $_SESSION['login'] = true;
+            header('location: ../menu.php');
+        }else{
+            //Mostra mensaje de que los datos no coinciden
+        }
 
     } 
 
     //Logeo de usuario
-    function login($nombre, $clave){
+    function registro($nombre, $clave, $confClave){
         
     }
 
+    //Mostrar usuario
+    function mostrarUsuario(){
+        
+    }
 ?>
