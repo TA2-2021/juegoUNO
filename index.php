f6a882f497c278a738174e74706ecb407fbafc8c
Neekomata <82986795+Neekomata@users.noreply.github.com>
2021-11-26 23:53:11 -0300

    Estoy podrido

--- index.php
+++ index.php
@@ -24,14 +24,12 @@
                     <input type="text" name="user" placeholder="Usuario" > 
                     <input type="password" name="pass" placeholder="Contraseña">
 
-                    <input class="boton"type="submit" value ="Mandale">
+                    <input id="ingresar" class="boton"type="submit" value ="Iniciar Sesion">
                     <a class="boton"href="registrarse.php">Registrarse</a>
                 </form>
             </div> 
 
 
     </section>
-
-
 </body>
 </html>
--- menu.php
+++ menu.php
@@ -37,7 +37,7 @@
                 <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">Tienda</a>
                 <a class="nav-link" id="v-pills-messages-tab" data-toggle="pill" href="#v-pills-messages" role="tab" aria-controls="v-pills-messages" aria-selected="false">Jugar</a>
                 <a class="nav-link" id="v-pills-settings-tab" data-toggle="pill" href="#v-pills-settings" role="tab" aria-controls="v-pills-personajes" aria-selected="false">Perfil</a>
-                <a class="nav-link" href="index.php" >Cerrar Sesion</a>
+                <a class="nav-link" href="php/logout.php" >Cerrar Sesion</a>
             </div>
 
             <!--Contenido de las opciones-->
