4cb187cfc1661e1a3944f9c2cc79c1eaf2dc69c8
Neekomata <82986795+Neekomata@users.noreply.github.com>
2021-11-27 20:56:37 -0300

    No toquen esto a menos que les diga

--- php/logout.php
+++ php/logout.php
@@ -1,12 +1,21 @@
 <?php
     include('conexion.php');
     session_start();
-
-    if ($conn-> connect_error)
-    {
-        
+    $usuario = $_SESSION['usuario'];
+    if ($conn-> connect_error){
+        die ("Error al conectar: ". $conn-> connect_error);
     }
 
+    $sql = "UPDATE usuario SET estado = 'Desconectado' WHERE username = '$usuario'";
+    $registro = $conn-> query($sql);
+
+    //!Borrar lo siguiente antes de enviar al Profe
+    //*Es para verificar que la Sentencia se ejecuta correctamente
+    if(isset($registro)){
+        echo "<script>alert('Actualizado')</script>";
+    }else{
+        echo "<script>alert('No actualizado')</script>";    
+    }
 
     session_destroy();
     header("Location: ../index.php");
